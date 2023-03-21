8e8db568997efba0f192a10845814a0fe9411ece
LucasFrancaRegistro <101057737+LucasFrancaRegistro@users.noreply.github.com>
2023-03-21 12:14:42 -0300

    update do vendedor

--- produto_crud.py
+++ produto_crud.py
@@ -1,5 +1,7 @@
 import pymongo
 from usuario_crud import createEndereso
+from compras_crud import search
+from vendedor_crud import sortVendedor
 client = pymongo.MongoClient("mongodb+srv://programa:o5ma5JcTMMNPbydk@cluster0.ephuxat.mongodb.net/?retryWrites=true&w=majority")
 database = client.test
 #print(db)
@@ -9,25 +11,38 @@ db = client.mercadolivre
 
 def insertProduto():
     global db
-    col = db.vendedor
+    col = db.produtos
     nome = input('nome do produto: ')
     quantidade = int(input('quantidade de produtos: '))
     preco = int(input('preço do produto: '))
     foto = input('link da foto do produto: ')
+    vendedores = search(sortVendedor())
+    escolha = int(input("Escolha o vendedor: "))
+    vendedor = vendedores[escolha]
+    del vendedor["produtos"]
     doc = {"nome": nome,
         "quantidade": quantidade,
         "preço": preco,
+        "vendedor": vendedor,
         "foto": foto}
     col.insert_one(doc)
 
 def updateProduto():
     global db
     col = db.produtos
-    query = { "nome": input("nome do produto a editar")}
+    produtos = search(sortProduto())
+    escolha = int(input("produto a editar"))
+    produto = produtos[escolha]
     campo = input("campo para editar ")
-    valor = input("valor novo ")
+    if campo == "vendedor":
+        vendedores = search(sortVendedor())
+        escolha = int(input("Escolha o vendedor: "))
+        vendedor = vendedores[escolha]
+        del vendedor["produtos"]
+        valor = vendedor
+    else:   valor = input("valor novo ")
     toUpdate = { "$set": { campo: valor} }
-    col.update_one(query, toUpdate)
+    col.update_one(produto, toUpdate)
 
 def sortProduto():
     global db
@@ -40,3 +55,7 @@ def deleteProduto():
     col = db.produtos
     query = { "nome": input("Nome do produto a deletar: ")}
     col.delete_one(query)
+
+# deleteProduto()
+# insertProduto()
+updateProduto()
